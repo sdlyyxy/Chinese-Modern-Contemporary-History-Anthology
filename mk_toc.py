a648f63a4eeaadb64b463b260ebd0a555d0674ef
sdlyyxy <sdlyyxy@icloud.com>
2017-10-29 16:12:15 +0800

    Add articles and improve README

--- mk_toc.py
+++ mk_toc.py
@@ -1,21 +1,30 @@
 import os
 _root = os.getcwd()
-dep=0
+dep = 0
 for root, dirs, files in os.walk('.'):
-    tab=''
+    tab = ''
     for c in root:
-        if c=='/':tab+='\t'
-    if root[:6]=='./.git':continue
-    if root=='./.vscode':continue
-    pos=0
-    for index,c in enumerate(root):
-        if c=='/':pos=index+1 
-    if root!='.':
-        print('%s* %s'%(tab[1:],root[pos:]))
+        if c == '/':
+            tab += '\t'
+    if root[:6] == './.git':
+        continue
+    if root == './.vscode':
+        continue
+    pos = 0
+    for index, c in enumerate(root):
+        if c == '/':
+            pos = index + 1
+    if root != '.':
+        print('%s* [%s](%s)' % (tab[1:], root[pos:], root))
     for name in files:
-        if name=='.DS_Store':continue
-        if name=='.gitignore':continue
-        if name=='mk_toc.py':continue
-        if name=='README.md':continue
-        if name=='tmp':continue
-        print ('%s* [%s](%s)' % (tab,name[:-3], root+'/'+name) )
+        if name == '.DS_Store':
+            continue
+        if name == '.gitignore':
+            continue
+        if name == 'mk_toc.py':
+            continue
+        if name == 'README.md':
+            continue
+        if name == 'tmp':
+            continue
+        print ('%s* [%s](%s)' % (tab, name[:-3], root + '/' + name))
